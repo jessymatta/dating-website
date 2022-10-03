9ea202b051a24dd69bf6473649f69126738081c6
jessymatta <jessy.w.matta@gmail.com>
2022-10-03 18:58:23 +0300

    Linking <> Users Login : retreived login input fields by their id

--- dating_frontend/scripts/login.js
+++ dating_frontend/scripts/login.js
@@ -1,5 +1,9 @@
 console.log("working?");
-
-
-
+// window.stop();
+//Getting login page input elements
+const username_input = document.getElementById("username");
+const password_input = document.getElementById("password");
+//Login button
+const login_btn = document.getElementById("login-btn");
+//Error div 
 
