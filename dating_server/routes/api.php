67b7c2f4cb74d201a0c87f551e3d5ec2baa2cf67
jessymatta <jessy.w.matta@gmail.com>
2022-10-03 06:57:19 +0300

    Backend <> Users SignUp/Login : added the middleware route in 'api.php'

--- dating_server/routes/api.php
+++ dating_server/routes/api.php
@@ -7,3 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function($router){
+
+});
