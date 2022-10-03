79c740d339ace6cfa123bad891418a042556ea37
jessymatta <jessy.w.matta@gmail.com>
2022-10-03 19:07:26 +0300

    Linking <> Users Login : added the api url to the login js file

--- dating_frontend/scripts/login.js
+++ dating_frontend/scripts/login.js
@@ -6,4 +6,10 @@ const password_input = document.getElementById("password");
 //Login button
 const login_btn = document.getElementById("login-btn");
 //Error div 
+const error_div = document.querySelector(".error-div-login");
+//Login api url
+const baseURL = "http://127.0.0.1:8000/api";
+const signup_url = baseURL + "/login"
+console.log(signup_url);
+
 
