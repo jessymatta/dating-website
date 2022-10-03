29008266396fec755c6305c16da6629c79d4e82b
jessymatta <jessy.w.matta@gmail.com>
2022-10-03 18:41:45 +0300

    Backend <> Users Login : removed the login route from the middleware

--- dating_server/routes/api.php
+++ dating_server/routes/api.php
@@ -11,6 +11,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function($router){
     Route::post('/register',[AuthController::class, 'register']);
-    Route::post('/login',[AuthController::class, 'login']);
+    // Route::post('/login',[AuthController::class, 'login']);
     Route::post('/logout',[AuthController::class, 'logout']);
 });
+
+Route::post('/login',[AuthController::class, 'login']);
