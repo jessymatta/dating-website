9ba7525f3f146523281219bf47dc7757a634cedf
jessymatta <jessy.w.matta@gmail.com>
2022-10-03 06:59:33 +0300

    Backend <> Users SignUp/Login : added the AuthController class to 'api.php'

--- dating_server/routes/api.php
+++ dating_server/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
