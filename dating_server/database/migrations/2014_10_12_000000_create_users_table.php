f980ea2771fd10d995589bc73079c82ed9e93bcf
jessymatta <jessy.w.matta@gmail.com>
2022-10-03 06:22:45 +0300

    Backend <> Users SingUp/Login : added the users table that will be migrated to the database

--- dating_server/database/migrations/2014_10_12_000000_create_users_table.php
+++ dating_server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        // users table
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->string('gender_id');
+            $table->string('interested_in');
+            $table->string('profile_picture')->nullable();
+            $table->string('location');
+            $table->date('birthdate');
+            $table->string('bio')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
