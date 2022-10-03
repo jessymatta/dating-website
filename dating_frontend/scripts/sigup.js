82a5e76d55f350460e5a8a9962a4b4938e3c436d
jessymatta <jessy.w.matta@gmail.com>
2022-10-03 13:55:37 +0300

    Linking <> Users SignUp : retreived all form inputs from their ids or classes

--- dating_frontend/scripts/sigup.js
+++ dating_frontend/scripts/sigup.js
@@ -1,1 +1,14 @@
-console.log("working?");
+//Getting signup page input elements
+const name_input = document.getElementById("name");
+const username_input = document.getElementById("username");
+const email_input = document.getElementById("email");
+const password_input = document.getElementById("password");
+const confirm_password_input = document.getElementById("password-confirm");
+const birthday_input = document.getElementById("birthday");
+const location_input = document.getElementById("location");
+const gender_input = document.getElementById("gender");
+const interested_in_input = document.getElementById("interested_gender");
+//Create account button
+const create_acc_btn = document.getElementById("create-acc-btn");
+//Error div 
+const error_div = document.querySelector(".error-div");
