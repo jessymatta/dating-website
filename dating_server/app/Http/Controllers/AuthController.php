1719bb8c7185d095868f19c8caae3e8fa1abb286
jessymatta <jessy.w.matta@gmail.com>
2022-10-03 07:10:59 +0300

    Backend <> Users SignUp/Login : imported some classes to the AuthController

--- dating_server/app/Http/Controllers/AuthController.php
+++ dating_server/app/Http/Controllers/AuthController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function __construct(){
         $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
+
+
 }
