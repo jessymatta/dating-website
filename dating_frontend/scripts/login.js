43277c2b929701b53c96732f442f7f043cd40709
jessymatta <jessy.w.matta@gmail.com>
2022-10-03 19:15:12 +0300

    Linking <> Users Login : stored the data in the input fields in a formDataobject

--- dating_frontend/scripts/login.js
+++ dating_frontend/scripts/login.js
@@ -1,5 +1,4 @@
 console.log("working?");
-// window.stop();
 //Getting login page input elements
 const username_input = document.getElementById("username");
 const password_input = document.getElementById("password");
@@ -12,4 +11,13 @@ const baseURL = "http://127.0.0.1:8000/api";
 const signup_url = baseURL + "/login"
 console.log(signup_url);
 
-
+// When the login button is clicked
+login_btn.addEventListener("click", () => {
+    error_div.classList.add('hide')
+    error_div.innerHTML = "";
+    //Take the inputs
+    const data = new FormData();
+    data.append('username', username_input.value);
+    data.append('password', password_input.value);
+    console.log(...data);
+});
