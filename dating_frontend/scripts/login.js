31b14ca72fea18c2027fc549b60d675e111c1dfb
jessymatta <jessy.w.matta@gmail.com>
2022-10-03 20:32:22 +0300

    Linking <> Users Login : added the login function that will post the retreived data to the login api

--- dating_frontend/scripts/login.js
+++ dating_frontend/scripts/login.js
@@ -1,4 +1,4 @@
-console.log("working?");
+window.stop();
 //Getting login page input elements
 const username_input = document.getElementById("username");
 const password_input = document.getElementById("password");
@@ -8,8 +8,30 @@ const login_btn = document.getElementById("login-btn");
 const error_div = document.querySelector(".error-div-login");
 //Login api url
 const baseURL = "http://127.0.0.1:8000/api";
-const signup_url = baseURL + "/login"
-console.log(signup_url);
+const login_url = baseURL + "/login"
+console.log(login_url);
+
+
+// Posting data using axios to the login api for validation
+const login = async (data) => {
+
+    try {
+        const response_login = await axios.post(login_url, data);
+        console.log(response_login);
+        localStorage.setItem('token', JSON.stringify(response_login.data.access_token))
+        localStorage.setItem('user', JSON.stringify(response_login.data.user))
+        // setTimeout(function () {
+        //     window.location.href = './home.html'
+        //   }, 1000)
+
+    } catch (error) {
+        console.log(error);
+        console.log(JSON.stringify(error.response.data));
+        error_div.append(JSON.stringify(error.message).replace(/[^a-zA-Z0-9 ]/g, '')+"---"+JSON.stringify(error.response.data).replace(/[^a-zA-Z0-9 ]/g, ' '));
+        error_div.classList.remove('hide');
+    }
+
+}
 
 // When the login button is clicked
 login_btn.addEventListener("click", () => {
@@ -20,4 +42,5 @@ login_btn.addEventListener("click", () => {
     data.append('username', username_input.value);
     data.append('password', password_input.value);
     console.log(...data);
+    login(data);
 });
