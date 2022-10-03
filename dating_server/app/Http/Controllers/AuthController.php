4163f2502a688f6029c0451a9ab47510245d29bf
jessymatta <jessy.w.matta@gmail.com>
2022-10-03 07:13:16 +0300

    Backend <> Users SignUp/Login : added the register function in the AuthController that will be used to sign up

--- dating_server/app/Http/Controllers/AuthController.php
+++ dating_server/app/Http/Controllers/AuthController.php
@@ -14,5 +14,41 @@ class AuthController extends Controller
         $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
+    public function register(Request $request){
+        $validator =Validator::make($request->all(),[
+            'name'=>'required',
+            'username'=>'required|string|unique:users',
+            'email'=>'required|string|email|unique:users',
+            'gender' => 'required',
+            'interested_in' => 'required',
+            'location' => 'required',
+            'birthdate'=>'required',
+            'password'=>'required|string|confirmed|min:6'
+
+        ]);
+
+        //if validation fails
+        if($validator->fails()){
+            return response() -> json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create([
+                'name'=> $request->name,
+                "email"=> $request->email,
+                "username"=> $request->username,
+                "gender"=>$request->gender,
+                "interested_in"=>$request->interested_in,
+                "location"=> $request->location,
+                "birthdate"=> $request->birthdate,
+                "password"=>Hash::make($request->password)
+            
+        ]);
+        
+        return response()->json([
+            'message'=>'User successfully registered',
+            'user'=>$user
+        ], 201);
+    }
+
 
 }
