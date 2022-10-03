8937ebab1d0e4c4f468a22ec92b72445a42ef9cf
jessymatta <jessy.w.matta@gmail.com>
2022-10-03 06:40:42 +0300

    Backend <> Users SignUp/Login : added more attributes to the  array to cover all the fields in the users db

--- dating_server/app/Models/User.php
+++ dating_server/app/Models/User.php
@@ -22,6 +22,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'username',
+        'gender',
+        'interested_in',
+        "location",
+        "birthdate",
     ];
 
     /**
