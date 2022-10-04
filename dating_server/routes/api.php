87af0ea8caf0795fce2b4d99f6dd630134509016
jessymatta <jessy.w.matta@gmail.com>
2022-10-04 10:08:37 +0300

    Fixing <> modified the api.php inside routes to fit proper coding conventions

--- dating_server/routes/api.php
+++ dating_server/routes/api.php
@@ -4,15 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+Route::group(['prefix' => 'v0.1'], function () {
+    Route::group(['middleware' => 'auth:api'], function ($router) {
+        Route::get('/add_favorite/{id}', [AuthController::class, 'addFavorite'])->name('add_favorite');
+        Route::get('/remove_favorite/{id}', [AuthController::class, 'removeFavorite'])->name('remove_favorite');
+    });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
-
-Route::group(['middleware' => 'jwt.auth', 'prefix' => 'auth'], function($router){
-    Route::post('/register',[AuthController::class, 'register']);
-    Route::get('add_favorite/{id}',[AuthController::class, 'addFavorite'])->name('add_favorite');
-    Route::get('remove_favorite/{id}',[AuthController::class, 'removeFavorite'])->name('remove_favorite');
-});
-
-Route::post('/login',[AuthController::class, 'login'])->name('login');
