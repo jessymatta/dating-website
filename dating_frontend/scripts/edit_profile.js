934bf0e7f8fff32b42617b6c6dfc5cdd965562fa
jessymatta <jessy.w.matta@gmail.com>
2022-10-04 19:09:01 +0300

    Frontend <> Users Homepage : added an edit profile modal

---
+++ dating_frontend/scripts/edit_profile.js
@@ -0,0 +1,25 @@
+//Edit profile modal
+const modalBtn = document.getElementById('edit-profile');
+const modalBg = document.querySelector('.modal-bg');
+const modalClose = document.querySelector('.modal-close');
+const modalSave = document.querySelector('.edit_profile_modal_save_btn');
+const client_name_input = document.getElementById('profile-input-name');
+const client_username_input = document.getElementById('profile-input-username');
+const client_email_input = document.getElementById('profile-input-email');
+
+const user_in_storage =JSON.parse(localStorage.getItem('user'));
+const client_id=user_in_storage.id;
+console.log(client_id);
+
+
+
+//When the button that should show the modal is clicked
+modalBtn.addEventListener('click', function () {
+    modalBg.classList.add('bg-active');
+    getProfile();
+})
+
+//When the X at the top left of the modal is clicked
+modalClose.addEventListener('click', function () {
+    modalBg.classList.remove('bg-active');
+})
