ebaa5f61f3e97ff23d8201cdf222e850b5bbf2a1
jessymatta <jessy.w.matta@gmail.com>
2022-10-04 20:00:34 +0300

    Frontend <> Users EditProfile : converted the uploaded image in the modal to a readable url

--- dating_frontend/scripts/edit_profile.js
+++ dating_frontend/scripts/edit_profile.js
@@ -6,6 +6,8 @@ const modalSave = document.querySelector('.edit_profile_modal_save_btn');
 const client_name_input = document.getElementById('profile-input-name');
 const client_username_input = document.getElementById('profile-input-username');
 const client_email_input = document.getElementById('profile-input-email');
+const input= document.getElementById("upload-pp");
+const profile_picture_div= document.getElementById("profile-div");
 
 const user_in_storage =JSON.parse(localStorage.getItem('user'));
 const client_id=user_in_storage.id;
@@ -16,10 +18,27 @@ console.log(client_id);
 //When the button that should show the modal is clicked
 modalBtn.addEventListener('click', function () {
     modalBg.classList.add('bg-active');
-    getProfile();
-})
+    // getProfile();
+});
 
 //When the X at the top left of the modal is clicked
 modalClose.addEventListener('click', function () {
     modalBg.classList.remove('bg-active');
+});
+
+//edit profile modal pp 
+input.addEventListener("change", (e) =>{
+    // console.log(e);
+    let image_file=e.target.files[0];
+    // console.log(image_file);
+    let reader = new FileReader;
+    // console.log(reader);
+    reader.readAsDataURL(image_file);
+    // console.log(reader);
+
+    reader.onload= (e) => {
+        let image_url=e.target.result;
+        profile_picture_div.src=image_url;
+    }
+
 })
